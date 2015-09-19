e111a886717149937855a941a4a80e427635df75
ewadkins <ewadkins83@gmail.com>
2015-09-19 14:41:56 -0400

    Adding starting location

--- routes/get_info.js
+++ routes/get_info.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var request = require('request');
+var fs = require('fs');
 var router = express.Router();
 
 
@@ -8,7 +9,18 @@ function queryString(query) {var str = ''; for (var key in query) {if (query.has
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	query["max_price"] = parseInt((req.query["budget"]),10);
+	var origin = iataCityCode(req.query.starting_location);
+	var max_price = parseInt((req.query["budget"]),10)
+	var query = {
+			apikey: 'NIht6DjAks1ZxOTkj2KWKRphIxhKxcGs',
+			origin: origin,
+			destination: null,
+			departure_date: '2015-10-06',
+			duration: 7,
+			direct: false,
+			max_price: max_price,
+			aggregation_mode: 'DESTINATION'
+	};
 	var url = 'http://api.sandbox.amadeus.com/v1.2/flights/inspiration-search' + queryString(query);
 	
 	request.get(url, function (error, response, body){
@@ -21,6 +33,58 @@ router.get('/', function(req, res, next) {
 	});
 });
 
+function getLocations(originStr) {
+	var origin = iataCityCode(originStr);
+	var query = {
+			apikey: 'NIht6DjAks1ZxOTkj2KWKRphIxhKxcGs',
+			origin: origin,
+			destination: null,
+			departure_date: '2015-10-06',
+			duration: 7,
+			direct: false,
+			max_price: 500,
+			aggregation_mode: 'DESTINATION'
+	};
+	var url = 'http://api.sandbox.amadeus.com/v1.2/flights/inspiration-search' + queryString(query);
+
+	request.get(url, function (error, response, body){
+		if (error) {
+			console.log(error);
+		}
+		else {
+			console.log(body);
+		}
+	});
+}
+
+function iataCityCode(str) {
+	str = str.replace(/[^\w\s]|_/g, '').replace(/\s+/g, ' ');
+	var terms = str.split(' ');	
+	var temp = fs.readFileSync('./IATACityCodes.txt').toString().split('\n');
+	var locations = [];
+	for (var i = 0; i < temp.length; i++) {
+		if (temp) {
+			locations.push(temp[i].replace(/^(\s*)|(\s*)$/g, '').replace(/\s+/g, ' '));
+		}
+	}
+	var code = null;
+	var bestMatchCount = 0;
+	for (var i = 0; i < locations.length; i++) {
+		var matchCount = 0;
+		for (var j = 0; j < terms.length; j++) {
+			if (locations[i].toLowerCase().match(terms[j].toLowerCase())) {
+				matchCount += 1;
+			}
+		}
+		if (matchCount > bestMatchCount) {
+			console.log(locations[i]);
+			code = locations[i].slice(locations[i].length - 3);
+			bestMatchCount = matchCount;
+		}
+	}
+	console.log(code);
+	return code;
+}
 
 module.exports = router;
 
--- script.js
+++ script.js
@@ -1,26 +1,61 @@
 
 var request = require('request');
+var fs = require('fs');
 
-var query = {
-		apikey: 'NIht6DjAks1ZxOTkj2KWKRphIxhKxcGs',
-		origin: 'BOS',
-		destination: null,
-		departure_date: '2015-10-06',
-		duration: 7,
-		direct: false,
-		max_price: 500,
-		aggregation_mode: 'DESTINATION'
-};
-var url = 'http://api.sandbox.amadeus.com/v1.2/flights/inspiration-search' + queryString(query);
+getLocations('Boston');
 
-request.get(url, function (error, response, body){
-	if (error) {
-		console.log(error);
+function getLocations(originStr) {
+	var origin = iataCityCode(originStr);
+	var query = {
+			apikey: 'NIht6DjAks1ZxOTkj2KWKRphIxhKxcGs',
+			origin: origin,
+			destination: null,
+			departure_date: '2015-10-06',
+			duration: 7,
+			direct: false,
+			max_price: 500,
+			aggregation_mode: 'DESTINATION'
+	};
+	var url = 'http://api.sandbox.amadeus.com/v1.2/flights/inspiration-search' + queryString(query);
+
+	request.get(url, function (error, response, body){
+		if (error) {
+			console.log(error);
+		}
+		else {
+			console.log(body);
+		}
+	});
+}
+
+function iataCityCode(str) {
+	str = str.replace(/[^\w\s]|_/g, '').replace(/\s+/g, ' ');
+	var terms = str.split(' ');	
+	var temp = fs.readFileSync('./IATACityCodes.txt').toString().split('\n');
+	var locations = [];
+	for (var i = 0; i < temp.length; i++) {
+		if (temp) {
+			locations.push(temp[i].replace(/^(\s*)|(\s*)$/g, '').replace(/\s+/g, ' '));
+		}
 	}
-	else {
-		console.log(body);
+	var code = null;
+	var bestMatchCount = 0;
+	for (var i = 0; i < locations.length; i++) {
+		var matchCount = 0;
+		for (var j = 0; j < terms.length; j++) {
+			if (locations[i].toLowerCase().match(terms[j].toLowerCase())) {
+				matchCount += 1;
+			}
+		}
+		if (matchCount > bestMatchCount) {
+			console.log(locations[i]);
+			code = locations[i].slice(locations[i].length - 3);
+			bestMatchCount = matchCount;
+		}
 	}
-});
+	console.log(code);
+	return code;
+}
 
 function queryString(query) {
 	var str = '';
