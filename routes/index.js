c3dd385047d223b74da1a67368f1457c13ecd299
Loc Trinh <loctrinh@mit.edu>
2015-09-19 18:56:50 -0400

    fixed conflict and add front page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'getAway' });
+	var today = new Date();
+	dateString = (today.getMonth()+1).toString() + "/" + today.getDate().toString()+"/"+today.getFullYear().toString();
+	res.render('index', { title: 'getAway' , date:dateString});
 });
 
 module.exports = router;
